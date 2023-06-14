fbf83fbe2477619f3427a85aad7bc0341cb8e098
yaarian-om <dip.kumar020@gmail.com>
2023-06-14 21:46:08 +0600

    Uploading AddBooks feature of Seller
    So, I have created a user 'Seller', I ensures the user is alive by a feature getIndex(Not included in Project Proposal) and created a feature called 'AddBooks' which is working fine.

--- interstellar_library/src/app.module.ts
+++ interstellar_library/src/app.module.ts
@@ -1,10 +1,13 @@
+import { SellerService } from './Seller/seller.service';
+import { SellerController } from './Seller/seller.controller';
+import { SellerModule } from './Seller/seller.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [SellerModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
