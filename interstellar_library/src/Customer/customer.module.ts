3526e043a83b531d1a6e3e302bfea28a75da950d
s-parvez <shakibparvez75@gnail.com>
2023-06-19 15:51:11 +0600

    Initializing Customer

---
+++ interstellar_library/src/Customer/customer.module.ts
@@ -0,0 +1,14 @@
+/*
+https://docs.nestjs.com/modules
+*/
+
+import { Module } from '@nestjs/common';
+import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
+
+@Module({
+    imports: [],
+    controllers: [CustomerController],
+    providers: [CustomerService],
+})
+export class CustomerModule { }
